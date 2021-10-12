7abfba279de29c850eee748bd28129544596f686
Yuri <yuri_mhb@yahoo.com.br>
2021-10-12 18:08:35 -0300

    Criando estrutura de identity.

---
+++ ECommerceMVC/Areas/Identity/Data/AppIdentityUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ECommerceMVC.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the AppIdentityUser class
+    public class AppIdentityUser : IdentityUser
+    {
+    }
+}
---
+++ ECommerceMVC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ECommerceMVC.Areas.Identity.Data;
+using ECommerceMVC.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ECommerceMVC.Areas.Identity.IdentityHostingStartup))]
+namespace ECommerceMVC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AppIdentityContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("AppIdentityContextConnection")));
+
+                services.AddDefaultIdentity<AppIdentityUser>()
+                    .AddEntityFrameworkStores<AppIdentityContext>();
+            });
+        }
+    }
+}
